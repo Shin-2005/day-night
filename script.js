e351ebe6bbedb8d392635e9ffd63de2044286d49
shin <z5483669@ad.unsw.edu.au>
2026-02-12 14:05:00 +1100

    finalised element positioning, added animations, combined index.html and night.html together, changed sun/moon into buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const openBtns = document.querySelectorAll(".open-modal-btn");
+const openModalBtns = document.querySelectorAll(".open-modal-btn");
 
-openBtns.forEach((btn) => {
+openModalBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     const targetId = "modal-" + btn.id;
     const targetModal = document.getElementById(targetId);
@@ -8,9 +8,29 @@ openBtns.forEach((btn) => {
   });
 });
 
-const closeBtns = document.querySelectorAll(".close-modal-btn");
-closeBtns.forEach((btn) => {
+const closeModalBtns = document.querySelectorAll(".close-modal-btn");
+closeModalBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
     btn.closest(".modal").classList.remove("open");
   });
+});
+
+const sunBtn = document.getElementById("sun");
+sunBtn.addEventListener("click", () => {
+  const daySection = document.getElementById("day-section");
+  const nightSection = document.getElementById("night-section");
+  const body = document.body;
+  body.id = "night";
+  daySection.style.display = "none";
+  nightSection.style.display = "block";
+});
+
+const moonBtn = document.getElementById("moon");
+moonBtn.addEventListener("click", () => {
+  const daySection = document.getElementById("day-section");
+  const nightSection = document.getElementById("night-section");
+  const body = document.body;
+  body.id = "day";
+  daySection.style.display = "block";
+  nightSection.style.display = "none";
 });
